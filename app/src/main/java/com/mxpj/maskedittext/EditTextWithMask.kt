cfd43c476f6105d60fc344ddd115ea08da9c2321
PuzhaikinMaxim <56976303+PuzhaikinMaxim@users.noreply.github.com>
2023-12-03 00:55:05 +0300

    Added right deletion detection

--- app/src/main/java/com/mxpj/maskedittext/EditTextWithMask.kt
+++ app/src/main/java/com/mxpj/maskedittext/EditTextWithMask.kt
@@ -23,12 +23,18 @@ class EditTextWithMask(
 
     private var turnOffFilterFlag = false
 
+    private var action = Action.ADD
+
+    private enum class Action {
+        ADD,
+        DELETE
+    }
+
     var unformattedValue = ""
 
     var suffix = ""
 
-    //# 2 1    /     2 0
-    var mask = ""
+    var mask = "miron pidoras (####) i huesos"
 
     var maskPlaceholderSymbol = '#'
 
@@ -39,20 +45,6 @@ class EditTextWithMask(
                 symbolPositions.add(index)
             }
         }
-
-        /*
-        var cumulativeRelativePositions = ArrayList<Int>()
-        var symbolPositionsPointer = 0
-        for(index in mask.indices){
-            cumulativeRelativePositions.add(index)
-            if(symbolPositions[symbolPositionsPointer] == index){
-                relativePositions.add(cumulativeRelativePositions)
-                cumulativeRelativePositions = ArrayList()
-                symbolPositionsPointer++
-            }
-        }
-
-         */
 
         var symbolPositionsPointer = 0
         for(char in mask){
@@ -72,31 +64,26 @@ class EditTextWithMask(
         val inputFilter = InputFilter { source, start, end, dest, dstart, dend ->
             if(turnOffFilterFlag) return@InputFilter source ?: ""
             val startRel = if(dstart < relativePositions.size) {
-                //println("relative pos start ${relativePositions[dstart]}")
                 relativePositions[dstart]
             }
             else {
-                //println(relativePositions.last() + (dstart - mask.length))
-                println("dstart $dstart mask ${mask.lastIndex}")
-                println("relative pos start ${relativePositions.last() + (dstart - mask.lastIndex)}")
                 relativePositions.last() + (dstart - max(mask.lastIndex, mask.length))
             }
+
             val endRel = if(dend < relativePositions.size) {
-                //println("relative pos end ${relativePositions[dend]}")
                 relativePositions[dend]
             }
             else {
-                //println(relativePositions.last() + (dend - mask.length))
-                //println("relative pos end ${relativePositions.last() + (dend - mask.lastIndex)}")
                 relativePositions.last() + (dend - max(mask.lastIndex, mask.length))
             }
-            if(dstart < dend /* && symbolPositions.contains(dend)*/){
-                unformattedValue = unformattedValue.substring(0, startRel) + unformattedValue.substring(endRel, unformattedValue.length)
+
+            unformattedValue = if(dstart < dend){
+                action = Action.DELETE
+                unformattedValue.substring(0, startRel) + unformattedValue.substring(endRel, unformattedValue.length)
+            } else {
+                action = Action.ADD
+                unformattedValue.substring(0, startRel) + source + unformattedValue.substring(endRel, unformattedValue.length)
             }
-            else {
-                unformattedValue = unformattedValue.substring(0, startRel) + source + unformattedValue.substring(endRel, unformattedValue.length)
-            }
-            //println(unformattedValue)
             source ?: ""
         }
         filters = filters.clone().plus(inputFilter)
@@ -104,18 +91,20 @@ class EditTextWithMask(
 
     private fun addTextChangedListener() {
         addTextChangedListener(object : TextWatcher {
+            var cursor: Int = 0
+
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
             override fun onTextChanged(text: CharSequence?, cursorPosition: Int, before: Int, count: Int) {
-
+                cursor = cursorPosition
             }
 
             override fun afterTextChanged(s: Editable?) {
                 this@EditTextWithMask.removeTextChangedListener(this)
                 turnOffFilterFlag = true
-                val mask = applyMask()
+                val mask = applyMask(cursor)
                 println(mask)
                 s?.let {
                     s.replace(0, s.length, mask)
@@ -126,24 +115,30 @@ class EditTextWithMask(
         })
     }
 
-    private fun applyMask(): String {
+    private fun applyMask(cursorPosition: Int): String {
         if(unformattedValue.isEmpty()) return ""
         val newString = mask.toCharArray()
-        //val addedCharIndexes = ArrayList<Int>()
         var newStringSuffix = unformattedValue
+        var lastFormattedPosition = 0
         for((unformattedPosition, formattedPosition) in symbolPositions.withIndex()){
             if(unformattedValue.length <= unformattedPosition) break
             newString[formattedPosition] = unformattedValue[unformattedPosition]
             newStringSuffix = newStringSuffix.substring(1)
-            //addedCharIndexes.add(unformattedPosition)
+            lastFormattedPosition = formattedPosition
         }
-        println("unformatted $unformattedValue")
-        if(newStringSuffix.isNotEmpty() /*unformattedValue.length > newString.size*/){
+        if(newStringSuffix.isNotEmpty()){
             return String(newString).plus(
                 newStringSuffix
             )
         }
-        return String(newString).split(maskPlaceholderSymbol)[0]
+        if(action == Action.ADD) {
+            return String(newString).split(maskPlaceholderSymbol)[0]
+        }
+        else {
+            return String(newString)
+                .split(maskPlaceholderSymbol)[0]
+                .substring(0,max(lastFormattedPosition, cursorPosition))
+        }
     }
 
     override fun onDraw(canvas: Canvas?) {
